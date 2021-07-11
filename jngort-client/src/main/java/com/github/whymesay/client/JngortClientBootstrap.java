36ed6bb76f54995683d6d18c9f95816d3373d83c
whymesay <18677253+whymesay@users.noreply.github.com>
2021-07-11 22:30:25 +0800

    init

---
+++ jngort-client/src/main/java/com/github/whymesay/client/JngortClientBootstrap.java
@@ -0,0 +1,11 @@
+package com.github.whymesay.client;
+
+/**
+ * @author caiyuanhao cyhyx521@gmail.com
+ * @date 2021/7/1122:19
+ */
+public class JngortClientBootstrap {
+    public static void main(String[] args) {
+
+    }
+}
